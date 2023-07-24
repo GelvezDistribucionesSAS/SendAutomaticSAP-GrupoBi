dc73de37cf219d8864119def901049bfa8ff0115
IIpapuII <wnserranoa@unadvirtual.edu.co>
2023-07-24 09:51:03 -0500

    Se Agregan  las consultas para el manejo de la casas generales de arauca.

---
+++ ScriptBitAutomatizacion/src/consultas/ConsultasArauca/inventarioGeneral.sql
@@ -0,0 +1,41 @@
+SELECT 
+CONCAT(max(TA.EndDate),'#', TA.CodeItem,'#',sum(TA.quanty),'#',TA.salpack) 
+from(
+select
+	(replace(CONVERT(varchar, getdate(), 11), '/', '')) as EndDate,  + REPLACE(REPLACE(RTRIM(REPLACE(RTRIM(LTRIM(ISNULL(CAST(ArtFicTec AS VARCHAR(249)), ''))), '', '')), CHAR(13), ''), CHAR(10), '') as item ,
+	convert( int,
+	isnull((
+SELECT
+		sum(CASE WHEN karnat = '+' THEN (karuni +(KARCAJ * KARARTEMB)) WHEN karnat = '-' THEN (karuni +(KARCAJ * KARARTEMB))* -1 END)
+	FROM
+		kardex K
+	LEFT JOIN Factura f ON
+		f.FacSec = k.FacSec
+	WHERE
+	f.FacFec BETWEEN '20200101' and '20230724'
+		AND k.SubBodSucCCSec = 2
+		and f.facest = 'A'
+		and artsec = a.artsec
+	group by
+		artsec),
+	0) ) as quanty,
+	(
+	SELECT
+		TOP 1 PREARTNOM
+	FROM
+		ArtPre AA
+	LEFT JOIN PresentacionArticulos PA ON
+		AA.preartcod = PA.preartcod
+	WHERE
+		AA.ARTSEC = A.ARTSEC) as salpack, a.ArtCod  as coded,
+		REPLACE(REPLACE(RTRIM(REPLACE(RTRIM(LTRIM(ISNULL(CAST(ArtCod AS VARCHAR(249)), ''))), '', '')), CHAR(13), ''), CHAR(10), '') as CodeItem
+from
+	Articulos a
+LEFT JOIN InventarioFamilia FF ON
+	FF.InvFamCod = A.InvFamCod
+LEFT JOIN InventarioSubgrupo SS ON
+	SS.InvSubGruCod = FF.InvSubGruCod
+where
+	InvGruCod IN('1') ) as TA
+
+GROUP by TA.item, TA.EndDAte, TA.salpack , TA.CodeItem
---
+++ ScriptBitAutomatizacion/src/consultas/ConsultasArauca/vendedoresGeneral.sql
@@ -0,0 +1,26 @@
+SELECT
+	T2.NitIde + '{' + v.VenNom + '{ {' + T2.NitIde
+FROM
+	MantisWebEv3.dbo.Vendedores v
+left join MantisWebEv3.dbo.Nit T2 on
+	T2.NitSec = v.VenNitSec
+WHERE
+	(
+	SELECT
+		sum(k.karvaltotMenDes)
+	FROM
+		MantisWebEv3.dbo.Kardex k
+	LEFT JOIN MantisWebEv3.dbo.Articulos t1 on
+		t1.ArtSec = k.ArtSec
+	LEFT JOIN MantisWebEv3.dbo.InventarioFamilia t2 on
+		t2.InvFamCod = t1.InvFamCod
+	LEFT JOIN MantisWebEv3.dbo.InventarioSubgrupo t3 on
+		t3.InvSubGruCod = t2.InvSubGruCod
+	LEFT JOIN MantisWebEv3.dbo.Factura t4 on
+		t4.FacSec = k.FacSec
+	WHERE
+		t4.FacTipTra in ('FDV','NCR')
+		AND t4.FacEst = 'A'
+		AND year(facfec)= 2023 AND month(facfec) = 7
+				AND t4.FacVenCod = V.VenCod
+				AND t3.InvGruCod IN ('1'))<> 0
